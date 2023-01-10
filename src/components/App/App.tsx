7d0f1a9ab705517726155defa1b40a1f155a528a
Fpsska <fpsska1337@gmail.com>
2023-01-10 12:07:52 +0300

    feat: install antd npm package, create Layout, Slider, Menu, Content markup in App.tsx component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { CopyOutlined } from '@ant-design/icons';
+
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+
+import type { MenuProps } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+type MenuItem = Required<MenuProps>['items'][number];
 
 import './App.css';
 import '../../assets/styles/_reset.scss';
@@ -6,10 +16,66 @@ import '../../assets/styles/style.scss';
 
 // /. imports
 
+function getItem(
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[]
+): MenuItem {
+    return {
+        key,
+        icon,
+        children,
+        label
+    } as MenuItem;
+}
+
+const items: MenuItem[] = [
+    getItem('Requests', 'sub1', <CopyOutlined />, [
+        getItem('request №1', '1'),
+        getItem('request №2', '2'),
+        getItem('request №3', '3')
+    ])
+];
+
 const App: React.FC = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer }
+    } = theme.useToken();
+
     return (
         <div className="App">
-            <h1>Hello world</h1>
+            <Layout style={{ minHeight: '100vh' }}>
+                <Sider
+                    collapsible
+                    collapsed={collapsed}
+                    onCollapse={value => setCollapsed(value)}
+                >
+                    <Menu
+                        theme="dark"
+                        defaultSelectedKeys={['1']}
+                        mode="inline"
+                        items={items}
+                    />
+                </Sider>
+                <Layout className="site-layout">
+                    <Content>
+                        <Breadcrumb style={{ padding: '16px' }}>
+                            <Breadcrumb.Item>Map</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <div
+                            style={{
+                                height: '100%',
+                                background: colorBgContainer
+                            }}
+                        >
+                            Leafleat map here
+                        </div>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>Footer</Footer>
+                </Layout>
+            </Layout>
         </div>
     );
 };
